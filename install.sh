cdfb08d32912186c54d7272731abb64b06f8f63e
Kamesh Magdum <kameshmagdum@gmail.com>
2023-01-10 20:47:27 +0530

    #

--- install.sh
+++ install.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
-#installing yay
+#installing paru
 sudo pacman -S --needed base-devel
 mkdir temp
 cd temp
-git clone https://aur.archlinux.org/yay.git
-cd yay
+git clone https://aur.archlinux.org/paru.git
+cd paru
 makepkg -si
 cd ..
 cd ..
 sudo rm -rf temp
 
-#updating using yay
-yay
+#updating using paru
+paru
 
 #installing apps
-yay -S neofetch timeshift thunar stacer firefox picom lxappearance rofi xfce4-power-manager polkit-gnome python-psutil mpd python-mpd2 gnome-keyring fzf python-cairo betterlockscreen
+paru -S neofetch timeshift thunar stacer firefox picom lxappearance rofi xfce4-power-manager polkit-gnome python-psutil mpd python-mpd2 gnome-keyring fzf python-cairo betterlockscreen
 
 #installing fonts
-yay -S nerd-fonts-jetbrains-mono noto-fonts noto-fonts-emoji ttf-linux-libertine ttf-dejavu
+paru -S nerd-fonts-jetbrains-mono noto-fonts noto-fonts-emoji ttf-linux-libertine ttf-dejavu
 
 #installing icons and themes
-yay -S papirus-icon-theme arc-gtk-theme
+paru -S papirus-icon-theme arc-gtk-theme
 
 #installing bluetooth
-yay -S bluez blueman
+paru -S bluez blueman
 sudo systemctl enable bluetooth.service
 
 #cleanup
 sudo pacman -Scc
 sudo pacman -Rns $(pacman -Qtdq)
 rm -rf ~/.cache/*
+paru -Scca
 
 #coping concigs
 cp -r configs/* ~/.config
